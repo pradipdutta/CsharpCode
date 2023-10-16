5b617e0cc6b43d83a0b0bbd21cf1ff3eb10d5fcd
Pradip Dutta <pradipdutta@msn.com>
2023-10-16 20:37:14 +0530

    triange Shape

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("This Is A Triangle Shape");
+int height = 5;
+for (int i=1; i<=height; i++)
+{
+    for (int j=1; j<=i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
